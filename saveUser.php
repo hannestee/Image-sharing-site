7a979c161e539f0a8cdf9d7962cd72d825ebf48b
spongefin <hannest@ety-a2123b-09.edu.wks.metropolia.fi>
2016-11-28 11:51:12 +0200

    asdasd
    asdasdasd

---
+++ saveUser.php
@@ -0,0 +1,37 @@
+<?php
+
+include_once("iheader.php");
+if(isset($_POST["submit"])){
+    $datat['username'] = $_POST['username'];
+    $datat['email'] = $_POST['email'];    
+    $datat['country'] = $_POST['contry'];
+    $datat['password'] = hash('sha256', $_POST['password']."ayy");
+    if(filter_var($datat['email'], FILTER_VALIDATE_EMAIL)) { 
+    try {
+        $stm = $DBH->prepare("INSERT INTO ga_users (username, password, email, country, title) VALUES (:username, :password, :email, :country, 'Newbie')");
+        if($stm->execute($datat)){
+                echo ("aa");
+                $_SESSION['username'] = $datat['username'];
+                $_SESSION['email'] = $datat['email'];
+                $_SESSION['country'] = $datat['country'];
+                $_SESSION['viesti'] = "Voit kirjautua" . $_SESSION['email'];
+                redirect("index.php");
+            } else {
+                $_SESSION['viesti'] = "en voi hyvin";
+                echo ("eee");
+                redirect("index.php");
+            }
+        }
+        catch(PDOException $e){
+            $_SESSION['viesti'] = "Tietokantaongelma"; //$e.getMessage()")
+            redirect("index.php");
+            echo ("sss");
+        }
+        } else {
+            echo("Huono sähköposti");
+            $_SESSION['viesti'] = "Väärä email";
+            redirect("index.php");
+    }
+}
+
+?>
